d923e0d3451e839ac01054b6a1d6a613fd3d4c24
Senak9 <joaomarcerlo.o.rocha@gmai.com>
2022-08-04 15:12:44 -0300

    feat: criando pokedex e componentes necessários

---
+++ src/component/cardType/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.css";
+import { useNavigate } from "react-router-dom";
+
+export default function CardType({ status }) {
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className={
+        status === "pokedex"
+          ? "pokedexBox containerCardType"
+          : status === "moves"
+          ? "movesBox containerCardType"
+          : status === "evolution"
+          ? "evolutionBox containerCardType"
+          : "locationsBox containerCardType"
+      }
+      onClick={() => {
+        navigate(status === "pokedex" ? "/pokedex" : "/");
+      }}
+    >
+      <h1>
+        {status === "pokedex"
+          ? "Pokedex"
+          : status === "moves"
+          ? "Moves"
+          : status === "evolution"
+          ? "Evolution"
+          : "Locations"}
+      </h1>
+    </div>
+  );
+}
---
+++ src/pages/pokedex/index.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Header from "../../component/header";
+import "./style.css";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ModalPokemon from "../../component/modalPokemon";
+import api from "../../api/api";
+import PokemonCard from "../../component/pokemonCard";
+
+export default function Pokedex() {
+  const [pokemonsList, setPokemonsList] = useState([]);
+
+  const getPokemonUrl = async (id) => {
+    const response = await api.get(`/pokemon/${id}`);
+    return response.data;
+  };
+
+  const listPokemon = async () => {
+    const list = [];
+    for (let i = 1; i <= 905; i++) {
+      const pokemon = await getPokemonUrl(i);
+      list.push(pokemon);
+    }
+
+    setPokemonsList(list);
+  };
+
+  listPokemon();
+
+  return (
+    <div className="containerPokedex">
+      <Header />
+      <div className="boxPokedex">
+        <h1>Pokedex Kanto</h1>
+        <div className="bodyPokedex">
+          {pokemonsList &&
+            pokemonsList.map((pokemon) => {
+              return (
+                <PokemonCard
+                  id={pokemon.id}
+                  name={pokemon.name}
+                  types={pokemon.types}
+                  pTypes={pokemon.types.map((typeInfo) => typeInfo.type.name)}
+                />
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
